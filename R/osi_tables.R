9a81e05285b15669fd85796638f6872f236f1685
Gerard <gerard.ros@nmi-agro.nl>
2023-02-17 09:12:06 +0100

    add table descriptions osi_countries

---
+++ R/osi_tables.R
@@ -0,0 +1,12 @@
+# tables with supportive information
+
+#' Table with country codes used in the package
+#'
+#' This table contains country names as used for the spatial extent where sptfs have been derived from.
+#'
+#' @format A data.frame with x rows and x columns:
+#' \describe{
+#'   \item{country_name}{the name of the country}
+#'   \item{country_code}{the country code;from https://datahub.io/core/country-codes}
+#'}
+"osi_countries"
